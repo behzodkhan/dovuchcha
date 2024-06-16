c438d29b8f520d8c931d203b52a5021f8eb14db0
behzodkhan <behzodmusurmonqulov02@gmail.com>
2024-06-16 23:17:32 +0500

    404

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Aloqa from './pages/Aloqa';
+import { NotFoundImage } from './pages/InvalidPage';
 
 function App() {
   return <MantineProvider>
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/aloqa' element={<Aloqa/>} />
+        <Route path='/*' element={<NotFoundImage/>} />
       </Routes>
     </BrowserRouter>
     <Analytics />
